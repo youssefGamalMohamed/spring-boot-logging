fb33fe920979aba9ce7fb17302e0a03427aa16a2
Youssef Gamal <youssef.gamal@intercom.com.eg>
2024-05-06 23:53:47 +0300

    chore: initial spring boot project with required maven dependecies ( spring-web, lombok )

---
+++ spring-boot-logging-app/src/test/java/org/youssef/gamal/springboot/logging/app/SpringBootLoggingAppApplicationTests.java
@@ -0,0 +1,13 @@
+package org.youssef.gamal.springboot.logging.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootLoggingAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
