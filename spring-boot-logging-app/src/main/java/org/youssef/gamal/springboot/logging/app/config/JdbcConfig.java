c88a97e64ad2e9bb0a705a717afaa9c2bb4f0ad0
Youssef Gamal <youssef.gamal@intercom.com.eg>
2024-05-08 13:45:37 +0300

    feat: Hikari Connection Pooling Added, If we want to use JdbcConfig.java with any Spring-Boot, Java Maven Projects will work with managed connection pooling for the db to maintain the stress on the db

--- spring-boot-logging-app/src/main/java/org/youssef/gamal/springboot/logging/app/config/JdbcConfig.java
+++ spring-boot-logging-app/src/main/java/org/youssef/gamal/springboot/logging/app/config/JdbcConfig.java
@@ -2,16 +2,35 @@ package org.youssef.gamal.springboot.logging.app.config;
 
 
 
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+
 import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class JdbcConfig {
 
 
+    public static int count = 0;
+    private static HikariConfig hikariConfig;
+    private static HikariDataSource hikariDataSource;
+
     public static Connection getConnection() throws SQLException {
-
-         return DriverManager.getConnection("jdbc:mysql://localhost:3306/logging_db", "root", "1234");
-
+        if(hikariConfig == null) {
+            hikariConfig = new HikariConfig();
+            hikariConfig.setJdbcUrl( "jdbc:mysql://localhost:3306/logging_db" );
+            hikariConfig.setUsername( "root" );
+            hikariConfig.setPassword( "1234" );
+            hikariConfig.setDriverClassName("com.mysql.cj.jdbc.Driver");
+        }
+        if(hikariDataSource == null) {
+            hikariDataSource = new HikariDataSource(hikariConfig);
+            count++;
+            System.out.println("A new Connection Created..... | count = " + count);
+        }
+        else {
+            System.out.println("A Connection Reused..... | " + count);
+        }
+        return hikariDataSource.getConnection();
     }
 }
